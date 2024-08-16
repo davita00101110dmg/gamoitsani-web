661107a684cd0880a269ab10229416eb1fbf52f3
Giorgi Kuliashvili <giorgi.kuliashvili.1@btu.edu.ge>
2024-08-17 00:18:51 +0400

    header finish

--- info/app.js
+++ info/app.js
@@ -1,8 +1,11 @@
 function toggleBurgerMenu(){
     const menu = document.querySelector(".burger-menu")
+    const overlay = document.querySelector('.overlay')
     menu.classList.toggle('active')
+    overlay.classList.toggle('active')
 }
 
 document.querySelector('.burger-menu-icon').addEventListener('click', toggleBurgerMenu)
 document.querySelector('.burger-menu-close').addEventListener('click', toggleBurgerMenu)
+document.querySelector('.overlay').addEventListener('click', toggleBurgerMenu)
 
